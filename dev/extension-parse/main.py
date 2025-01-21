a6509fb8be3f41a40a48536b4e1fa470c6e0ac90
Remy Huang <chinaguyan@gmail.com>
2025-01-21 13:09:38 +0800

    update: dev

--- dev/extension-parse/main.py
+++ dev/extension-parse/main.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
-import execjs
+import logging
+import json
 from pprint import pprint
 
 
@@ -22,7 +23,7 @@ def get_ext_files(path):
     ext_files = []
 
     # for files in extention root folder
-    for root, dirs, files in os.walk(ext_folder_path):
+    for root, _, files in os.walk(ext_folder_path):
         for file_name in files:
             file_path = os.path.join(root, file_name)
             if file_name.endswith(".js") and is_extention_file(file_path):
@@ -32,16 +33,37 @@ def get_ext_files(path):
 
 
 def parse_ext(ext_path):
-    pass
+    # call node script to parse extension
+    # get parsed json in text form
+    ext_json_txt = subprocess.run(
+        ["node", "./ext-parser.js", ext_path], capture_output=True, text=True
+    ).stdout
+    
+    # DEV
+    logging.debug(ext_json_txt)
+    
+    # converst text to json object
+    ext = json.loads(ext_json_txt)
+    
+    ret={}
+    
+    
+    return ret
 
 
 if __name__ == "__main__":
-    ext_folder_path = "./ccw_extensions/custom-extension/extensions/"
+    logging.basicConfig(level=logging.DEBUG,filename='parse.log', filemode='w')
+    # DEV:
+    # cd ./dev/extension-parse; python main.py
+    # DEV: single extension folder for test /dannydev or /DollyPro or /QuakeStudio
+    ext_folder_path = "./ccw_extensions/custom-extension/extensions/QuakeStudio"
     ext_repo_path = "./ccw_extensions/custom-extension"
 
     update_repo(ext_repo_path)
 
     ext_files = get_ext_files(ext_folder_path)
+    logging.debug(ext_files)
 
-    with open("./temp-ext-list.js", "w") as f:
-        f.write(f"const window.ext_files = {str(ext_files)};")
+    for ext_file in ext_files:
+        parsed_ext = parse_ext(ext_file)
+
