311399566bb4c6cc6a8171ef5726688b83eb27ef
Remy Huang <chinaguyan@gmail.com>
2025-02-23 21:23:08 +0800

    add: pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [{
+    script: 'npm start',
+  }],
+
+  deploy: {
+    production: {
+      key: 'key.pem',
+      user: 'site',
+      host: '139.196.178.129',
+      ref: 'origin/main',
+      repo: 'https://github.com/remyhuang03/sja-v3',
+      path: '/home/site',
+      'pre-deploy-local': '',
+      'post-deploy': 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options':"ForwardAgent=yes"
+    }
+  }
+};
