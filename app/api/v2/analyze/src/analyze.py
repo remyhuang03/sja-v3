4936f24e3952337007ce84a1e9cb45b7bab1dbc3
Remy Huang <chinaguyan@gmail.com>
2025-07-26 03:21:29 +0800

    new: ccw extensions update

--- app/api/v2/analyze/src/analyze.py
+++ app/api/v2/analyze/src/analyze.py
@@ -5,7 +5,7 @@ from .AnalyzeReport import AnalyzeReport
 from .block_info import *
 
 # 内核版本号
-CORE_VERSION = "analyze-7.3.2"
+CORE_VERSION = "analyze-7.3.3"
 # 分析报告
 report = AnalyzeReport(CORE_VERSION)
 
